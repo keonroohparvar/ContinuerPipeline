841b085ff8268f9cfd31e10b5bc8776cb878de7c
Keon <kroohpar@gmail.com>
2022-10-27 16:59:25 -0700

    Created class for losses

---
+++ diffusion_model/loss.py
@@ -0,0 +1,16 @@
+"""
+This contains the class implementation of the loss function, and allows the user to specify a 
+loss function to use.
+"""
+
+class LossFunction:
+    def __init__(self, loss_type='l1'):
+        self.loss_type = loss_type
+    def get_loss(self, noise, noise_pred):
+        # L1 Loss
+        if self.loss_type == 'l1':
+            return F.l1_loss(noise, noise_pred)
+        # Raise error if loss type is not one of the supported ones
+        else:
+            raise NotImplementedError()
+            
