eb44bf09f67ec8b602193c07d335f9d33119230d
Keon <kroohpar@gmail.com>
2022-10-27 16:52:16 -0700

    First implementation of whole model architecture

---
+++ diffusion_model/time_embedding.py
@@ -0,0 +1,21 @@
+"""
+This contains the class implementation of the class that we will use for time embedding.
+"""
+
+import math
+import torch
+
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        # TODO: Double check the ordering here
+        return embeddings
---
+++ diffusion_model/unet_block.py
@@ -0,0 +1,35 @@
+"""
+This contains the code for a simple block in the U-Net architecture.
+"""
+
+from torch import nn
+
+
+class Block(nn.Module):
+    def __init__(self, in_channel, out_channel, time_embedding_dim, up=False):
+        super.__init__()
+        self.time_mlp =  nn.Linear(time_embedding_dim, out_channel)
+        if up:
+            self.conv1 = nn.Conv2d(2*in_channel, out_channel, 3, padding=1)
+            self.transform = nn.ConvTranspose2d(out_channel, out_channel, 4, 2, 1)
+        else:
+            self.conv1 = nn.Conv2d(in_channel, out_channel, 3, padding=1)
+            self.transform = nn.Conv2d(out_channel, out_channel, 4, 2, 1)
+        self.conv2 = nn.Conv2d(out_channel, out_channel, 3, padding=1)
+        self.bnorm1 = nn.BatchNorm2d(out_channel)
+        self.bnorm2 = nn.BatchNorm2d(out_channel)
+        self.relu  = nn.ReLU()
+    
+    def forward(self, x, t):
+        # First Conv
+        h = self.bnorm1(self.relu(self.conv1(x)))
+        # Time embedding
+        time_emb = self.relu(self.time_mlp(t))
+        # Extend last 2 dimensions
+        time_emb = time_emb[(..., ) + (None, ) * 2]
+        # Add time channel
+        h = h + time_emb
+        # Second Conv
+        h = self.bnorm2(self.relu(self.conv2(h)))
+        # Down or Upsample
+        return self.transform(h)
